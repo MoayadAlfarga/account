2d1edd6ee26a60343c2992a61bbb77a964c198c7
Moayad Faraj <moayadalfarga.20@gmail.com>
2026-03-31 15:19:07 +0300

    add Docker

--- src/main/java/com/accounts/client/LoanClient.java
+++ src/main/java/com/accounts/client/LoanClient.java
@@ -1,4 +1,14 @@
 package com.accounts.client;
 
+import com.accounts.dto.LoanDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "", url = "")
 public interface LoanClient {
+    @GetMapping("")
+    LoanDto getLoanById(@PathVariable Long id);
+    @GetMapping("")
+    LoanDto getLoanByAccountId(@PathVariable Long accountNumber);
 }
